428edda53bcf00d108ac5f2138020d7a84cde7f4
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-05-08 12:10:47 +0200

    Solve problem 2.02

---
+++ Arithmetic/02.pl
@@ -0,0 +1,5 @@
+prime_factors(N, L) :- prime_factors(N, 2, L).
+
+prime_factors(1, _, []).
+prime_factors(N, D, [D|Ds]) :- N mod D =:= 0, Q is (N div D), prime_factors(Q, D, Ds).
+prime_factors(N, D, Es) :- E is (D + 1), prime_factors(N, E, Es).
